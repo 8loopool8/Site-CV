2342f4c6ab6245271252cdc3a42159a4ba106e97
8loopool8 <lupuandrei123123@gmail.com>
2026-02-01 05:58:46 +0200

    Surgical: Logo scale 1.8 + translate-y-3 (Tacos Alignment)

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -12,9 +12,9 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 18593, hash: 'bb56b0d277ecb65bdfb6dca5da99c63a1d0498e66103c60b35f910db1eacab56', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 9940, hash: '2ef04d851efb0d62e606d63a160c92eb517667e0c52d4297c29acbf9a5b212d8', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 72571, hash: 'ef13fc140cb9d203814153f00b717c5f06e571f135caf5cc76cba95bac9b3bdd', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
-    'styles-MFIQG7GB.css': {size: 53257, hash: 'a5dYgYFX9tY', text: () => import('./assets-chunks/styles-MFIQG7GB_css.mjs').then(m => m.default)}
+    'index.csr.html': {size: 18593, hash: 'a9b98d08261db38ae8100249097962dad4db2ae1f8f0dd806a3410076c1f4b10', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 9940, hash: '7028e9c9a4e153ba3e6b71d61c6106e44e391a3b57a90ae8dc6df4baa1861101', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 72697, hash: '5bb21916acfbfa547bc962eb232dc1f13f581619debde20ff21218814307df7b', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'styles-2ZDAMMVA.css': {size: 53369, hash: '2sHZEyY4jrY', text: () => import('./assets-chunks/styles-2ZDAMMVA_css.mjs').then(m => m.default)}
   },
 };
